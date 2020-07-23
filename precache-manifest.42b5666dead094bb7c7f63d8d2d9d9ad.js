f8300e1dfb9e1cf35212aa9d3482945cf6b8909a
matteo-buonastella <matteobuonastella@gmail.com>
2020-07-23 14:03:47 -0400

    Updates

---
+++ precache-manifest.42b5666dead094bb7c7f63d8d2d9d9ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e83825164f045ae2893b285c5e7363",
+    "url": "/Recipe-Web-App/index.html"
+  },
+  {
+    "revision": "f932d9cd85bd83e3318f",
+    "url": "/Recipe-Web-App/static/css/2.da92d159.chunk.css"
+  },
+  {
+    "revision": "3f3b3830c7ca850da19a",
+    "url": "/Recipe-Web-App/static/css/main.6ab1d61a.chunk.css"
+  },
+  {
+    "revision": "f932d9cd85bd83e3318f",
+    "url": "/Recipe-Web-App/static/js/2.d9fdd4bf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Recipe-Web-App/static/js/2.d9fdd4bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f3b3830c7ca850da19a",
+    "url": "/Recipe-Web-App/static/js/main.c2430872.chunk.js"
+  },
+  {
+    "revision": "1d622438eb337f059f7a",
+    "url": "/Recipe-Web-App/static/js/runtime-main.43845502.js"
+  }
+]);
